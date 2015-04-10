6a2ff7def9a337752de049171777fe20e8b29b91
Nick Becker <nbeck90@uw.edu>
2015-04-09 21:42:42 -0700

    Initialize city swap project in Django and created the base/home html templates.

---
+++ cityswap/cityswap/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^$', 'cityswap.views.home_page', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+]
