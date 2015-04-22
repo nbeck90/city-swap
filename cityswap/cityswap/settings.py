208af10df8f01c8edf6b4690d7468729c594417b
Nick Becker <nbeck90@uw.edu>
2015-04-22 13:38:16 -0700

    Add registration

--- cityswap/cityswap/settings.py
+++ cityswap/cityswap/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'profiles',
+    'registration',
 )
 
 MIDDLEWARE_CLASSES = (
--- cityswap/cityswap/urls.py
+++ cityswap/cityswap/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^$', 'cityswap.views.home_page', name='home'),
+    url(r'^accounts/', include('registration.backends.default.urls')),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
