24bbc4ade1379724522290493a713d09f445aa69
Nick Becker <nbeck90@uw.edu>
2015-04-29 23:55:11 -0700

    Add static directories

--- cityswap/cityswap/settings.py
+++ cityswap/cityswap/settings.py
@@ -85,6 +85,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'cityswap/static')
+]
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'cityswap/templates'),
