c5b63c89fbb7e387c3607fa659cb7d36e7db402a
Nick Becker <nbeck90@uw.edu>
2015-04-30 18:21:13 -0700

    Add Bootstrap

--- cityswap/cityswap/urls.py
+++ cityswap/cityswap/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 
 urlpatterns = [
@@ -11,3 +12,5 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
